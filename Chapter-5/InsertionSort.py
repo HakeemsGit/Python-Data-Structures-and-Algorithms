44a8e345623008902ceb187e6c1db0f30e2b52fb
HakeemsGit <82256619+HakeemsGit@users.noreply.github.com>
2021-08-21 21:30:08 -0500

    Added Insertion Sort algorithm

--- Chapter-5/InsertionSort.py
+++ Chapter-5/InsertionSort.py
@@ -1,12 +1,23 @@
+# [5, 4, 3, 2, 1]
+# [4, 5, 3, 2, 1]
+# [3, 4, 5, 2, 1]
+# [2, 3, 4, 5, 1]
+# [1, 2, 3, 4, 5]
 def insertion_sort(lst):
     """
-    Sort a list of comparable
-    elements into ascending order
+    :param lst: A list of comparable Objects
+    Sort the list into ascending order.
+    Best case:  O(N)
+    Worst case: O(N^2)
     """
-    for index in range(1, len(lst)):                                     # Iterate from 1 - (length - 1)
-        current = lst[index]                                             # Current element to be inserted
-        correct_index = index                                            # Find the correct index for the element
-        while correct_index > 0 and lst[correct_index - 1] > current:
-            lst[correct_index] = lst[correct_index - 1]
-            correct_index -= 1
-        lst[correct_index] = current                                     # Current is now in the proper index
+    for k in range(1, len(lst)):                # iterate from 1 to n-1
+        current = lst[k]                        # current element to be inserted
+        j = k                                   # find correct index, j, for insertion
+        while j > 0 and lst[j-1] > current:     # elements lst[j-1] must be after current element
+            lst[j] = lst[j-1]
+            j -= 1
+        lst[j] = current                        # assign current to the correct location
+        print(lst)
+
+
+insertion_sort([5, 4, 3, 2, 1])
